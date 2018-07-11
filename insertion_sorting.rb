5b2a3b6ef62ed249046fd9837ecbfab4e8520508
rawenfalls <zaqmond@yandex.ru>
2018-07-12 00:04:51 +0300

    v0.02

--- insertion_sorting.rb
+++ insertion_sorting.rb
@@ -1,17 +1,10 @@
 input=gets.chomp.strip.scan(/[1-9]+/)
-output=[]
-output<<input.shift
-loop do
-  break if input == []
-  output.unshift(input.shift)
-  for i in 0..output.length
-    if output[i+1]!=nil
-      if output[i]>output[i+1]
-        output[i],output[i+1]=output[i+1],output[i]
-      else
-        break
-      end
+for i in 1..input.length-1
+  if input[i]<input[i-1]
+    for e in 0..i
+      input[i-e],input[i-1-e]=input[i-1-e],input[i-e]
+      break if input[i-1-e]>input[i-2-e] or i-2-e<0
     end
   end
 end
-puts output.join(" ")
+puts input.join(" ")
